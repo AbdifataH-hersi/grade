3a4569015c0ee55f82dde2a34044d0529337e2c5
AbdifataH-hersi <mohamedabdifatah107@gmail.com>
2024-09-14 03:12:54 +0900

    grading exercise

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hello");
+
+let grade =90;
+
+if (grade >= 90 && grade <=100) {
+
+ console.log("You got an A grade")
+
+}else if (grade >=75 && grade < 90) {
+
+ console.log("You got a B grade")
+
+} else if (grade >=65 && grade < 75){
+
+    console.log("You got a C grade")
+
+}else if (grade >=60 && grade < 75){
+
+ } else if (grade < 60) {
+        console.log('You got a FAILED');
+ }
